cc344ae3039464abd7ed240979d8952482bbce73
Yahia Abukhalil <yahiamrvc@gmail.com>
2023-10-25 23:44:49 +0300

    Working TSP using random shuffles and visualize the best route

--- sketch.js
+++ sketch.js
@@ -11,11 +11,19 @@ function setup() {
         var point = createVector(random(width), random(height));
         cities[i]= point;
     }
+    var dist = calcDistance(cities);
+    recordDistance = dist;
+    bestPath = cities.slice();
 
 }
 
 function draw() {
     background(0);
+    fill(255);
+    noStroke();
+    textSize(12);
+    text(`Best: ${floor(recordDistance)}`, 340, 10);
+  
 
     fill(255);
     for (var i = 0; i < cities.length; i++) {
@@ -33,6 +41,39 @@ function draw() {
     endShape();
 
 
+    stroke(255, 0, 0);
+    strokeWeight(4);
+    noFill();
+    beginShape();
+    for(var i = 0;i<cities.length;i++){
+        vertex(bestPath[i].x,bestPath[i].y);
+    }
+    endShape();
+
+    var randI = floor(random(cities.length));
+    var randJ = floor(random(cities.length));
+    swap(cities , randI , randJ);
+
+    var dist = calcDistance(cities);
+  if (dist < recordDistance) {
+    recordDistance = dist;
+    bestPath = cities.slice();
+  }
 
 
+}
+
+function swap(arr , i , j){
+    var temp = arr[i];
+    arr[i] = arr[j];
+    arr[j]= temp;
+}
+
+function calcDistance(points){
+    var sum = 0;
+    for(var i = 0; i< points.length - 1; i++){
+        var d = dist(points[i].x,points[i].y,points[i+1].x,points[i+1 ].y);
+        sum+= d;
+    }
+    return sum;
 }
